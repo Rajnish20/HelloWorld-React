707b7441c2af94549333b3c81f528d5a51e030b9
Rajnish20 <rajnishsahu95@gmail.com>
2021-08-02 13:00:46 +0530

    Add-display hello from bridgelabz using input name and implement onChange

--- src/App.js
+++ src/App.js
@@ -7,17 +7,28 @@ class App extends React.Component{
   constructor(){
     super()
     this.state={
-      title:'Hello There ! From BridgeLabz'
+      title:'Hello There ! From BridgeLabz',
+      username:''
     }
   }
   onClick = ($event) =>{
     window.open('https://unsplash.com/s/photos/anime',"_blank")
   }
+
+  onNameChange = (event) => {
+    console.log("Value is ", event.target.value);
+    this.setState({username:event.target.value})
+  }
   render(){
     return (
+      <div className="app-main">
       <div>
-        <h1>{this.state.title}</h1>
+        <h1>Hello {this.state.username} from BridgeLabz</h1>
         <img onClick={this.onClick} src={image} alt=""/>
+      </div>
+      <div>
+        <input placeholder="Enter Your Name" onChange={this.onNameChange} />
+      </div>
       </div>
     );
   }
